11ce0e4c701862393a9a4ad340e470e557ebbe65
mxber2022 <mxber2022@gmail.com>
2024-09-08 04:37:05 +0200

    ENS

--- zksnark/packages/app/pages/meta.tsx
+++ zksnark/packages/app/pages/meta.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useSDK } from "@metamask/sdk-react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { AvatarResolver, utils as avtUtils } from '@ensdomains/ens-avatar';
+import { ethers } from "ethers";
 
 export const METAMASK = () => {
   const [account, setAccount] = useState<string | null>(null);
@@ -22,10 +24,76 @@ export const METAMASK = () => {
     }
   };
 
+  const [avaURI, setAvaURI] = useState("")
+  const [image, setImage] = useState("");
+  const [nameResolved, setNameResolved] = useState("");
+
+  async function resolveNames(name: any) {
+    console.log("name:",name);
+    if(name) {
+    setAvaURI("")
+    const tempProvider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/e96abcff2f494bcd81fadc53c8fd6ac9");
+    const lastFourDigits = name.substring(name.length - 4);
+
+    if(lastFourDigits === ".eth") 
+    {
+      setAvaURI("");
+      var address =  await tempProvider.resolveName(name);
+      console.log("resolveNames: ", address);
+      //@ts-ignore
+      setNameResolved(address);
+    }
+    else {
+      try
+      {
+        console.log("else:");
+        setAvaURI("");
+        //@ts-ignore
+        setNameResolved("");
+        console.log(tempProvider);
+        const resolvedAddress = await tempProvider.lookupAddress(name);
+        console.log("resolvedAddress: ", resolvedAddress);
+        //@ts-ignore
+        setNameResolved(resolvedAddress);
+
+        /* 
+          Resolve Avatar
+        */
+       //@ts-ignore
+        const avt = new AvatarResolver(tempProvider);
+        //@ts-ignore
+        const avatarURI = await avt.getAvatar(resolvedAddress);
+        console.log("AVA URI: ", avatarURI);
+        //@ts-ignore
+        setAvaURI(avatarURI);
+      }
+      catch(e) {
+        
+      }
+        
+    }
+  }
+        
+  }
+  if(nameResolved==null){
+    setNameResolved(account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : "")
+  }
+
+  useEffect(()=>{
+
+    try{
+      resolveNames(account);
+    } 
+    catch(e) {
+      console.log("cannot resolve name");
+    }
+
+  },[account] )
+
   return (
     <div className="c">
     <button className="cbtm" onClick={connect} disabled={connecting || loading}>
-      {account ? "Connected" : "Connect Metamask"}
+      {account ? `Connected: ${nameResolved}` : "Connect Metamask"}
     </button>
   
 </div>
